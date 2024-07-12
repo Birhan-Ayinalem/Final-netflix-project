333bf6d6537b9535fe00f502ff6a3896df4396f6
Birhan-Ayinalem <ayichewbirhanayinalem.com>
2024-07-12 15:17:32 +0300

    netflix footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
-// import Footer from "../../components/Footer/Footer";
+import Footer from "../../components/Footer/Footer";
 // import RowList from "../../components/Row/RowList/RowList";
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
       <Header />
       <Banner />
       {/* <RowList /> */}
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
